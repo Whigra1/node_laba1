576894d5ba5350607e31e8a5ea278bbb2b2a8ecb
Whigra1 <balacsasha@gmail.com>
2019-10-24 23:01:29 +0300

    ddd

--- server.py
+++ server.py
@@ -13,12 +13,16 @@ def index():
     [bot.send_message(int(chat_id),"File was comitted") for chat_id in ids]
     try:
         payload = loads(request.data)
-        data += "Login: " + payload['pusher']['name'] + '\n'
-        data += "Message: " + payload['commits'][0]['message'] +"\n"
-        data += "Timestamp: " + payload['commits'][0]['timestamp'] + '\n'
-        data += "Modified files: " + str(payload['commits'][0]['modified'])
-        data += "Modified files: " + str(payload['commits'][0]['added'])
-        [bot.send_message(int(chat_id), data) for chat_id in ids]
+        for i in payload['commits']:
+            data += f"Commit{i + 1}"
+            data += "Login: " + payload['pusher']['name'] + '\n'
+            data += "Message: " + payload['commits'][i]['message'] +"\n"
+            data += "Timestamp: " + payload['commits'][i]['timestamp'] + '\n'
+            data += "Modified files: " + str(payload['commits'][i]['modified'])
+            data += "Modified files: " + str(payload['commits'][i]['added'])
+            data += "Modified files: " + str(payload['commits'][i]['removed'])
+            data += "-"*25
+            [bot.send_message(int(chat_id), data) for chat_id in ids]
     except Exception as e:        
         [bot.send_message(int(chat_id),"Can't get data.") for chat_id in ids]
 
