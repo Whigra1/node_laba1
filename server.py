7f3a8978d7736499c2459f6eec88942fba5fb28f
Whigra1 <balacsasha@gmail.com>
2019-10-24 23:06:13 +0300

    FINAL

--- server.py
+++ server.py
@@ -13,14 +13,14 @@ def index():
     [bot.send_message(int(chat_id),"File was comitted") for chat_id in ids]
     try:
         payload = loads(request.data)
-        for i in payload['commits']:
+        for commit_num, data_dict in enumerate(payload['commits']):
             data += f"Commit{i + 1}"
             data += "Login: " + payload['pusher']['name'] + '\n'
-            data += "Message: " + payload['commits'][i]['message'] +"\n"
-            data += "Timestamp: " + payload['commits'][i]['timestamp'] + '\n'
-            data += "Modified files: " + str(payload['commits'][i]['modified'])
-            data += "Modified files: " + str(payload['commits'][i]['added'])
-            data += "Modified files: " + str(payload['commits'][i]['removed'])
+            data += "Message: " + data_dict['message'] +"\n"
+            data += "Timestamp: " + data_dict['timestamp'] + '\n'
+            data += "Modified files: " + str(data_dict['modified'])
+            data += "Modified files: " + str(data_dict['added'])
+            data += "Modified files: " + str(data_dict['removed'])
             data += "-"*25
             [bot.send_message(int(chat_id), data) for chat_id in ids]
     except Exception as e:        
