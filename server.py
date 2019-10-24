b19f013d1547821d8a579b85ecaf53e68e66c022
Whigra1 <balacsasha@gmail.com>
2019-10-24 22:56:29 +0300

    This is test message.

--- server.py
+++ server.py
@@ -13,10 +13,10 @@ def index():
     [bot.send_message(int(chat_id),"File was comitted") for chat_id in ids]
     try:
         payload = loads(request.data)
-        data += payload['pusher']['name'] + '\n'
-        data += payload['commits'][0]['message'] +"\n"
-        data += payload['commits'][0]['timestamp'] + '\n'
-        data += str(payload['commits'][0]['modified'])
+        data += "Login: " + payload['pusher']['name'] + '\n'
+        data += "Message: " + payload['commits'][0]['message'] +"\n"
+        data += "Timestamp: " + payload['commits'][0]['timestamp'] + '\n'
+        data += "Modified files: " + str(payload['commits'][0]['modified'])
         [bot.send_message(int(chat_id), data) for chat_id in ids]
     except Exception as e:        
         [bot.send_message(int(chat_id),"Can't get data.") for chat_id in ids]
