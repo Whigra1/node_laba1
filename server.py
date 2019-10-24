a7aa9fab72bd6f8d9a35eb7d411be4829c2a2bac
Whigra1 <balacsasha@gmail.com>
2019-10-24 23:10:46 +0300

    FINAL FINAL

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def index():
-    print("success")
     ids = csvhandler.read()
     data = ""
     [bot.send_message(int(chat_id),"File was comitted") for chat_id in ids]
@@ -21,7 +20,7 @@ def index():
             data += "Modified files: " + str(data_dict['modified']) + '\n'
             data += "Modified files: " + str(data_dict['added'])+ '\n'
             data += "Modified files: " + str(data_dict['removed'])+ '\n'
-            data += "-"*25 + '\n'
+            data += "-" * 25 + '\n'
             [bot.send_message(int(chat_id), data) for chat_id in ids]
     except Exception as e:        
         [bot.send_message(int(chat_id),"Can't get data.") for chat_id in ids]
